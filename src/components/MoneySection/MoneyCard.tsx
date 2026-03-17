6109bf130b3247bf6b34a26c5c5c4722330c6493
Kevin Telles <kevinrgtelles@gmail.com>
2026-03-17 00:52:52 -0300

    feat: improve border

--- src/components/MoneySection/MoneyCard.tsx
+++ src/components/MoneySection/MoneyCard.tsx
@@ -13,22 +13,20 @@ export default function MoneyCard({ title, value, icon, accentColor }: MoneyCard
   const Icon = icon
 
   return (
-    <Box bg="#16202a" borderRadius="lg" overflow="hidden">
-      <Box borderLeft={`4px solid ${accentColor}`} p={4}>
-        <Flex justify="space-between" align="start" mb={6}>
-          <Text fontSize="0.75rem" lineHeight="1rem" color={accentColor} textTransform="uppercase" letterSpacing="0.16em" fontWeight="800">
-            {title}
-          </Text>
-          <Icon size={18} color={accentColor} style={{ opacity: 0.8 }} />
-        </Flex>
+    <Box bg="#16202a" borderRadius="lg" overflow="hidden" border={`1px solid ${accentColor}`} p={4}>
+      <Flex justify="space-between" align="start" mb={6}>
+        <Text fontSize="0.75rem" lineHeight="1rem" color={accentColor} textTransform="uppercase" letterSpacing="0.16em" fontWeight="800">
+          {title}
+        </Text>
+        <Icon size={18} color={accentColor} style={{ opacity: 0.8 }} />
+      </Flex>
 
       <HStack justifyContent="space-between"> 
         <Text fontSize="2.25rem" lineHeight="2.5rem" fontWeight="900" textAlign="center">
           {value}
         </Text>
         <MoneyInput value={value} />
-        </HStack>
-      </Box>
+      </HStack>
     </Box>
   )
 }
